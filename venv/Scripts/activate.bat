ce57eb832676d75fd10366a846a5fd83374f3c3c
EricaSmalla <EricaLSmalla@gmail.com>
2025-03-14 13:27:38 -0400

    Your commit message

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\mikib\OneDrive\Documents\GitHub\NeuroLearn-AI\venv"
+set "VIRTUAL_ENV=C:\Users\mikib\OneDrive\Desktop\NeuroLearn-AI\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
